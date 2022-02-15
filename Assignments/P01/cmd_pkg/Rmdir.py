b2ea8b9fb8e315a5600dfbdfcd711885abe76d92
apwarren <allywarren7@gmail.com>
2022-02-15 09:37:02 -0600

    updating history + typos

--- Assignments/P01/cmd_pkg/Rmdir.py
+++ Assignments/P01/cmd_pkg/Rmdir.py
@@ -1,5 +1,4 @@
-from .Rm import rm
-
+import os
 def rmdir(params):
   """   
     NAME
@@ -8,20 +7,22 @@ def rmdir(params):
         rmdir DIRECTORY
     DESCRIPTION
         This command takes a given directory and permanently removes it. 
-        The command can delete multiple directories at a time if desired
-        as well as takes wild card commands such that it will perform
-        a partial match to directory names. This flag will recursively 
-        delete everything inside of the directory and then once its 
-        contents have been emptied it will delete the directory as well.
+        The command can delete any empty directory and remove it.
     USAGE
         rm DIRECTORY
-          --Delete DIRECTORY from the system
+          --Delete DIRECTORY from the system if it is empty
         rm DIRECTORY1 DIRECTORY2
-          --Delete both DIRECTORY1 and DIRECTORY2
+          --Delete both DIRECTORY1 and DIRECTORY2 if they are empty
   """
-  #Pass in every given directory name and call the remove function recursively
-  direct = params
-  #direct.insert(0, '-r')
-  #rm will delete the directory and all its contents
-  rm(direct)
+  #Pass in every given directory name and remove all that exist
+  directs = params
+  for direct in directs:
+    try:
+        if len(os.listdir(direct)) == 0:
+          #Directory is empty and can be deleted
+          os.rmdir(direct)
+        else:
+          print('rmdir: \'' + direct + '\'Directory is not empty: Cannot be removed')
+    except Exception:
+      print('rmdir: \'', direct, '\': Directory does not exist')
   return
